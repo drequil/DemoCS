270c36b541b338d6cb9caf55597c7b7c16fdca0e
drequil <andre.gott@checkmarx.com>
2015-08-04 14:55:52 -0500

    Repository Creation

---
+++ Demo.cs
@@ -0,0 +1,147 @@
+namespace ns1
+{
+
+    class WebPage
+    {
+                public virtual void foo()
+                {
+                                string s = "100002";
+                                string s2 = "999999";
+                }
+    }
+
+    class MyWebPage : WebPage
+    {
+         public override void foo()
+                {
+                }
+     }
+
+    class MyClass
+    {
+        void foo(string s)
+                {              
+                                WebPage p1 = new WebPage();
+                                MyWebPage p2 = new MyWebPage();
+                                
+                                p1.foo();
+                                p2.foo();
+
+
+                                a=3;b=4;c=5;
+                                if (s > "xyz")
+                                {
+                                                foo("hello");
+                                }
+                                if (s <= "xyz")
+                                {
+                                                print("good");
+                                                print("bye");
+                                }
+                                if (x==y)
+                                                print("z");
+                }
+        void boo()
+        {
+                                if ( a = 3)
+                                {
+                                }
+                                if (3 == a)
+                                {
+                                }
+                                if (a == a)
+                                {
+                                }
+                                if (3 == 3)
+                                {
+                                }
+            int a = "zz - hello world";
+            foo(a);
+            x = 3;
+            y = x + 1;
+            z = y + 2;
+        }
+
+        void abc(string inp)
+        {
+
+
+            string s = prms.getParameterValues("Sharp");
+            string s2 = fix(s);
+
+            execute(s);
+            execute(s2);
+
+            s = s2;
+            execute(s);
+            execute(s2);
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//              string s = input(); // Todo
+//              string s2 = fix(s);
+//              string s3 = s + s2;
+
+//              execute(s);
+//              execute (s2);
+//              execute (s3);
+
+//              s = s2;
+//              execute(s);
+//              execute (s2);
+//              execute (s3);
+
+//              s3 = s2;
+//              execute(s);
+//              execute (s2);
+//              execute (s3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
